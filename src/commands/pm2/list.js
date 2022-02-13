6fbc47ea046d73e261263161484b266bbafaaf4b
Shadow <thebotoftheshadow@gmail.com>
2022-02-12 23:08:46 -0600

    ope

--- src/commands/pm2/list.js
+++ src/commands/pm2/list.js
@@ -2,18 +2,18 @@ const { MessageEmbed } = require("discord.js")
 const { rawList } = require("../../pmtwo")
 
 module.exports = {
-    command: {
-      name: "list",
-      description: "List processes",
-    },
-    permissions: [],
-    run: async (interaction, client) => {
-      await interaction.deferReply()
-      let allP = rawList()
+  command: {
+    name: "list",
+    description: "List processes",
+    adminGuild: true,
+  },
+  permissions: [],
+  run: async (interaction, client) => {
+    await interaction.deferReply()
+    let allP = rawList()
 
-      let embed = new MessageEmbed().setTitle(`Process List`).setDescription(`\`\`\`bash\n${allP}\`\`\``)
+    let embed = new MessageEmbed().setTitle(`Process List`).setDescription(`\`\`\`bash\n${allP}\`\`\``)
 
-      interaction.editReply({embeds: [embed]})
-    },
-  }
-  
+    interaction.editReply({ embeds: [embed] })
+  },
+}
--- src/commands/pm2/process.js
+++ src/commands/pm2/process.js
@@ -7,6 +7,7 @@ module.exports = {
       name: "process",
       description: "Get process info",
       defaultPermission: true,
+      adminGuild: true,
       options: [
           {
               type: "STRING",
